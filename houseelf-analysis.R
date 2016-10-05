14d1d8180828c050806f604c9136289171225390
Bruno Morandin Figueiredo <Bruno Morandin Figueiredo>
2016-10-04 21:23:45 -0500

    Data imported into the script. 10/3/2016

---
+++ houseelf-analysis.R
@@ -0,0 +1,6 @@
+# ---------- Problem 2 ----------
+# The objective of this analysis is to try to correlate house elfs magic power to their ear size and the precentage of 'g'and 'c' base pair in their DNA.
+
+# ---------- Problem 3 ----------
+# 3 Import data into R
+read.csv("./data/houseelf_earlength_dna_data.csv")
