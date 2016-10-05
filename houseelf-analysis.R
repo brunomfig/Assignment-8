63560c57bc6ef18bba5c5f1ff3c71b476220c6d9
Soil Fertility <Soil Fertility>
2016-10-05 08:32:59 -0500

    Added a function to calculate gc content of dna sequences. 10/5/2016

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -3,4 +3,16 @@
 
 # ---------- Problem 3 ----------
 # 3 Import data into R
-read.csv("./data/houseelf_earlength_dna_data_1.csv")
+elf_1 <- read.csv("./data/houseelf_earlength_dna_data_1.csv")
+
+# ---------- Problem 6 ----------
+# 1 Function to calculate 'g' and 'c' base pairs content in a given DNA sequence
+gc_content <- function(dnaseq) {
+  l <- str_to_lower(dnaseq)
+  t <- str_length(l)
+  g <- str_count(l, "g")
+  c <- str_count(l, "c")
+  gc <- round((g + c) / t * 100, 2)
+  return(gc)}
+
+gc_content(elf_1$dnaseq)
